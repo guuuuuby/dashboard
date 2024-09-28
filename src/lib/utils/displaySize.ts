a7e458ea46b82a7164266a7815320cc33193d75e
cactus1488 <cactus1488@icloud.com>
2024-09-28 12:38:38 +0300

    refactor utils dir

--- src/lib/utils/displaySize.ts
+++ src/lib/utils/displaySize.ts
@@ -1,10 +1,3 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
 export function displaySize(bytes: number): string {
   const prefixes = ['', 'К', 'М', 'Г', 'Т', 'П'];
   let index = 0;
