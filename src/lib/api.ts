b6ef21caa5f5303f1166e3be1f9107bfb3903dc2
cactus1488 <cactus1488@icloud.com>
2024-10-01 22:25:07 +0300

    change api base url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
 import type { App } from 'guby';
 import { treaty } from '@elysiajs/eden';
 
-export const api = treaty<App>(new URL('/api', location.href).href);
+export const api = treaty<App>(new URL(import.meta.env.DEV ? '/api' : '/', location.href).href);
