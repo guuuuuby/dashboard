9681ef0d2e5d9e81503a4985a629d6b985354a16
cactus1488 <cactus1488@icloud.com>
2024-10-04 21:51:34 +0300

    add client download links on /

---
+++ src/lib/downloadOptions.ts
@@ -0,0 +1,6 @@
+export const downloadOptions = [
+  { icon: 'tabler:brand-apple-filled', label: 'macOS (Apple Silicon)', link: '/guby-macos-arm' },
+  { icon: 'tabler:brand-apple-filled', label: 'macOS (Intel)', link: '/guby-macos-x64' },
+  { icon: 'fluent-mdl2:linux-logo-32', label: 'Linux', link: '/guby-linux' },
+  { icon: 'tabler:brand-windows', label: 'Windows', link: '/guby-win32.exe' },
+];
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,48 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Separator } from '@/components/ui/separator';
 import { Input } from '@/components/ui/input';
+import { Icon } from '@iconify/vue';
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
+import { downloadOptions } from '@/lib/downloadOptions';
 
 const id = ref('');
-const router = useRouter()
+const router = useRouter();
 </script>
 
 <template>
-  <main class="max-w-xs min-h-[100dvh] mx-auto flex justify-center items-center">
+  <main class="max-w-xs min-h-[100dvh] mx-auto flex flex-col gap-6 justify-center items-center">
     <form class="flex gap-2" @submit.prevent="router.push(`/${id}`)">
       <Input required placeholder="ID" class="font-mono" v-model="id" />
       <Button>Перейти</Button>
     </form>
+    <Separator label="або" />
+    <DropdownMenu>
+      <DropdownMenuTrigger as-child>
+        <Button class="gap-1" variant="secondary">
+          <Icon icon="tabler:download" class="text-xl" />
+          Завантажити клієнт
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Опції завантаження</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem v-for="{ icon, label, link } in downloadOptions" class="gap-1" as-child>
+          <a :href="link" target="_blank">
+            <Icon :icon class="text-xl" />
+            {{ label }}
+          </a>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   </main>
 </template>
