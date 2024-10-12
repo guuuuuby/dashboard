b37d4761627a6cbd7bc06701847a19548bd48c62
cactus1488 <cactus1488@icloud.com>
2024-10-12 19:50:05 +0300

    create terminal frontend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,24 @@
 <script setup lang="ts">
-import DownloadOptionPicker from '@/components/DownloadOptionPicker.vue';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Separator } from '@/components/ui/separator';
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
+import DownloadOptionPicker from "@/components/DownloadOptionPicker.vue";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 
-const id = ref('');
+const id = ref("");
 const router = useRouter();
 </script>
 
 <template>
-  <main class="max-w-xs min-h-[100dvh] mx-auto flex flex-col gap-6 justify-center items-center">
-    <form class="flex gap-2" @submit.prevent="router.push(`/${id}`)">
-      <Input required placeholder="ID" class="font-mono" v-model="id" />
-      <Button>Перейти</Button>
-    </form>
-    <Separator label="або" />
-    <DownloadOptionPicker />
-  </main>
+    <main
+        class="max-w-xs min-h-[100dvh] mx-auto flex flex-col gap-6 justify-center items-center"
+    >
+        <form class="flex gap-2" @submit.prevent="router.push(`/${id}`)">
+            <Input required placeholder="ID" class="font-mono" v-model="id" />
+            <Button>Перейти</Button>
+        </form>
+        <Separator label="або" />
+        <DownloadOptionPicker />
+    </main>
 </template>
--- src/views/dashboard/TerminalView.vue
+++ src/views/dashboard/TerminalView.vue
@@ -1,41 +1,101 @@
 <script setup lang="ts">
-import '@xterm/xterm/css/xterm.css';
+import { api } from '@/lib/api';
+import { useSessionId } from '@/lib/composables/useSessionId';
 import { Terminal } from '@xterm/xterm';
-import { useTemplateRef } from 'vue';
-import { onMounted } from 'vue';
-import { ref } from 'vue';
+import { FitAddon } from '@xterm/addon-fit';
+import '@xterm/xterm/css/xterm.css';
+import { onMounted, useTemplateRef, ref } from 'vue';
+import { useDebounceFn } from '@vueuse/core';
 
+const sessionId = useSessionId();
 const terminalContainer = useTemplateRef('terminal');
+const title = ref("Під'єднання до віддаленого термінала...");
+
+const syncTerminalSize = useDebounceFn(
+  (columns: number, lines: number) => {
+    api.terminal.put({
+      sessionId: sessionId.value,
+      event: {
+        action: 'sync',
+        columns,
+        lines,
+      },
+    });
+  },
+  100,
+  { maxWait: 500 }
+);
 
 onMounted(() => {
+  const ws = new WebSocket(`/live?sessionId=${sessionId.value}&channel=terminal`);
+  ws.binaryType = 'arraybuffer';
+
   const container = terminalContainer.value;
   if (!container) return;
 
-  const terminal = new Terminal({windowOptions:{}});
-  let command: string[] = [];
+  const fitAddon = new FitAddon();
+  const terminal = new Terminal();
   terminal.open(container);
-  terminal.write('> ');
-  
-  terminal.onData((data) => {
-    if (data === '\u007F') {
-      if (command.length > 0) {
-        terminal.write('\b \b');
-        command.pop();
-      }
-    } else if (data === '\r') {
-      terminal.write('\n\r> ');
-      command = [];
-    } else {
-      terminal.write(data);
-      command.push(data);
-    }
+  terminal.loadAddon(fitAddon);
+  fitAddon.fit();
+
+  const resizeObserver = new ResizeObserver(() => fitAddon.fit());
+  resizeObserver.observe(container, { box: 'content-box' });
+
+  terminal.onTitleChange((newTitle) => {
+    title.value = newTitle;
   });
+  terminal.onResize(({ cols, rows }) => syncTerminalSize(cols, rows));
+
+  terminal.writeln("Під'єднання до віддаленого термінала...");
+
+  ws.addEventListener('open', () => {
+    terminal.writeln('Очікування відповіді від клієнта...');
+    title.value = 'Очікування відповіді від клієнта...';
+    api.terminal.put({
+      sessionId: sessionId.value,
+      event: {
+        action: 'open',
+        columns: terminal.cols,
+        lines: terminal.rows,
+      },
+    });
+
+    terminal.onData((data) => {
+      if (ws.readyState !== WebSocket.OPEN) return;
+
+      ws.send(data);
+    });
+
+    ws.addEventListener('message', (event) => {
+      const data = new Uint8Array(event.data);
+      terminal.write(data);
+    });
+  });
+
+  return () => {
+    ws.close();
+    terminal.dispose();
+    fitAddon.dispose();
+    resizeObserver.disconnect();
+    api.terminal.put({
+      sessionId: sessionId.value,
+      event: { action: 'close' },
+    });
+  };
 });
 </script>
 
 <template>
-  <h2 class="font-bold text-2xl">Термінал</h2>
-  <div class="rounded-xl border overflow-hidden">
-    <div ref="terminal" class="p-2 bg-black"></div>
-  </div>
+  <section class="rounded-xl border overflow-hidden h-full">
+    <div class="border-b px-2 py-1 flex justify-center items-center">
+      <div class="[&>*]:aspect-square [&>*]:h-4 [&>*]:rounded-full flex h-full gap-2">
+        <div class="bg-zinc-200 dark:bg-zinc-800"></div>
+        <div class="bg-zinc-200 dark:bg-zinc-800"></div>
+        <div class="bg-zinc-200 dark:bg-zinc-800"></div>
+      </div>
+      <h2 class="flex-1 text-center">{{ title }}</h2>
+    </div>
+    <div ref="terminal" class="p-2 w-full h-full bg-black"></div>
+  </section>
 </template>
